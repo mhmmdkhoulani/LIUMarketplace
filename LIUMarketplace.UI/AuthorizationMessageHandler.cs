0fd5a9865ef766ed8b87e8405a065997fe2b4172
Mohammad <mhmmdkholani@gmail.com>
2022-05-15 14:13:30 +0300

    Add HttpClient and Authorization message handler

---
+++ LIUMarketplace.UI/AuthorizationMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.LocalStorage;
+
+public class AuthorizationMessageHandler : DelegatingHandler
+{
+    private readonly ILocalStorageService _localStorage;
+
+    public AuthorizationMessageHandler(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
+    protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+
+        if (await _localStorage.ContainKeyAsync("access_token"))
+        {
+            var token = await _localStorage.GetItemAsStringAsync("access_token");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        }
+        Console.WriteLine("Authorization message handler called");
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- LIUMarketplace.UI/Program.cs
+++ LIUMarketplace.UI/Program.cs
@@ -1,11 +1,23 @@
+using Blazored.LocalStorage;
 using LIUMarketplace.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("LIUMarketPlace.Api", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7081/");
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+builder.Services.AddTransient<AuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("LIUMarketPlace.Api"));
+
+
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
